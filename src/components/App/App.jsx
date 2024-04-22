9794ce9a5115af4dd5b6082ae3fe46677307c12f
Ryno Myburgh <rynomyburgh@gmail.com>
2024-04-22 09:23:56 +0200

    Update App component to use CompletionForm in new branch

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import CompletionForm from '../CompletionForm';
+
+const App = () => {
+    return (
+        <div className="App">
+            <CompletionForm />
+        </div>
+    );
+};
+
+export default App;
